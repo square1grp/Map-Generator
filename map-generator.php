d51c02f1e869d656ee82f99218e978d682425fa1
square1grp <developer01@square1grp.com>
2020-07-04 11:13:02 -0700

    init

---
+++ map-generator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Plugin Name: Map Generator
+* Description: This is a Map Generator
+* Version: 1.0
+* Author: Emmanuel Lao
+* Author URI: https://square1grp.github.io/
+**/
+
+add_shortcode( 'map-generator', function() {
+  echo "Map Generator";
+} );
