565f8706140e8896e06fedcfa0ae442bf067b6e1
square1grp <developer01@square1grp.com>
2020-07-09 03:12:35 -0700

    xls/xlsx file read

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,14 +47,14 @@
     $tableWrapper[0].el = sourceEle;
 
     // overlay text
-    let overlayTxt = $("<em />", {
+    let $overlayTxt = $("<em />", {
       "class": "tableize tableize-overlay",
       html: sourceEle.title || "click to copy/paste"
     });
 
-    $tableWrapper.append(overlayTxt);
+    $tableWrapper.append($overlayTxt);
 
-    overlayTxt[0].tbl = $table;
+    $overlayTxt[0].tbl = $table;
 
     // add table before text area
     $(sourceEle).hide().before($tableWrapper);
@@ -70,7 +70,7 @@
         $(this).remove()
       })
       .on("scroll", function () {
-        $(overlayTxt).css("top", $(this).scrollTop())
+        $($overlayTxt).css("top", $(this).scrollTop())
       });
 
     // paste event
@@ -82,7 +82,7 @@
             c(sourceEle),
             unValidateSource(),
             !1
-      } catch (f) {
+      } catch (err) {
         console.log("clipboard access failed")
       }
       return !0
@@ -107,11 +107,166 @@
     }
   };
 
-  var unValidateSource = function () {
+  // init source data
+  var unValidateSource = function () { };
 
+
+  var dragFileCancelEvent = function (e) {
+    e.stopPropagation();
+    e.preventDefault()
+  }
+
+  var dragFileStart = function (e) {
+    dragFileCancelEvent(e);
+    let $tableWrapper = $("#sourceWrap div.tableize");
+    let $overlayTxt = $("#sourceWrap em.tableize");
+    $tableWrapper.addClass("tableize-drag");
+    $overlayTxt.text("drop your file here");
+  };
+
+  var dragFileReset = function (e, is_error = false) {
+    if (e) dragFileCancelEvent(e);
+
+    let $sourceWrap = $("#sourceWrap");
+    let $tableWrapper = $sourceWrap.find("div.tableize");
+    let $overlayTxt = $sourceWrap.find("em.tableize");
+
+    $sourceWrap.next(".drag-drop-error").remove();
+    if (is_error) {
+      var $error = $('<p class="drag-drop-error">').html("&#9888; Sorry, there was a problem loading your file. Try copy & pasting your location data instead.");
+      $sourceWrap.after($error)
+    }
+    $tableWrapper.removeClass("tableize-drag");
+    $tableWrapper.removeClass("tableize-drop");
+    $overlayTxt.text($("#sourceData").attr("title"))
+  };
+
+  var readDragFileTxt = function (file, callback) {
+    readDragFile(file, callback, "readAsText")
+  };
+
+  var readDragFileUrl = function (file, callback) {
+    readDragFile(file, callback, "readAsDataURL")
+  };
+
+  var readDragFile = function (file, callback, read_type) {
+    let fr = new FileReader;
+    fr.onload = function (file) {
+      callback(file.target.result)
+    };
+    fr.onerror = function () {
+      dragFileReset(null, "Unable to read file data.")
+    };
+    fr[read_type](file)
+  };
+
+  var dragFileReadyCSV = function (csvData) {
+    let csvArray = csvToArray(csvData);
+
+    csvArray.length > 1 && csvArray[0].length > 1 && (csvData = arrayToCsv(csvArray, "\t"));
+
+    dragFileReady(csvData);
+  }
+
+  var dragFileReadyXLS = function (xlsFile) {
+    if (!(typeof xlsFile.ext == "undefined" || typeof xlsFile.file_content == "undefined"))
+      try {
+        let xlsLibrary = window[xlsFile.ext];
+        let workbook = xlsLibrary.read(xlsFile.file_content.split(";base64,")[1], {
+          type: "base64"
+        });
+        let file_content = xlsLibrary.utils.sheet_to_csv(workbook.Sheets[workbook.SheetNames[0]], {
+          FS: "\t"
+        });
+
+        dragFileReady(file_content)
+      } catch (err) {
+        dragFileReset(null, "Unable to convert file data into CSV format: " + err.message)
+      }
+  };
+
+  var dragFileReady = function (csvData) {
+    if (csvData.length) {
+      var $sourceData = $("#sourceData");
+      $sourceData.val(csvData);
+
+      MapGeneratorTableize($sourceData.get(0));
+
+      unValidateSource();
+      $("div.tableize").addClass("tableize-unhover");
+
+      $(window).focus(function () {
+        $("div.tableize").mouseover(function () {
+          $(this).removeClass("tableize-unhover");
+          $(this).off("mouseover");
+          $(window).off("focus")
+        })
+      })
+    }
+
+    dragFileReset();
+  };
+
+  var dragFileLoading = function (e) {
+    dragFileCancelEvent(e);
+
+    let $tableWrapper = $("#sourceWrap div.tableize");
+    let $overlayTxt = $("#sourceWrap em.tableize");
+
+    $tableWrapper.addClass("tableize-drop");
+    $overlayTxt.html("&hellip;loading&hellip;");
+
+    try {
+      let file = e.originalEvent.dataTransfer.files[0];
+      let file_type = file.type;
+
+      if (file_type) {
+        if (file_type == "text/tab-separated-values" || /\.(csv|tsv|tab)$/i.test(file.name)) {
+          file_type = "text/csv";
+        } else if (/\.xls$/i.test(file.name)) {
+          file_type = "application/vnd.ms-excel";
+        }
+      }
+
+      if (file_type == "text/csv")
+        readDragFileTxt(file, function (file_content) {
+          dragFileReadyCSV(file_content)
+        });
+      else {
+        let xlsFile = {
+          ext: file_type == "application/vnd.ms-excel" ? "XLS" : "XLSX"
+        };
+
+        readDragFileUrl(file, function (file_content) {
+          xlsFile.file_content = file_content;
+          dragFileReadyXLS(xlsFile)
+        })
+      }
+    } catch (err) {
+      dragFileReset(null, "Unable to load file data: " + err.message);
+    }
+  };
+
+  // source text area drag & drop events
+  var initSourceDragDrop = function () {
+    let $sourceWrap = $("#sourceWrap");
+    let $sourceData = $("#sourceData");
+
+    if ($sourceWrap.length) {
+      $sourceWrap.prev("h1").text("Paste your location data below to map it:");
+      $sourceWrap.parent().find(".edit-method").text("paste or drag it");
+      $sourceData.attr("title", "click to copy/paste, or drop your file here");
+      $sourceWrap.find("em.tableize").text($sourceData.attr("title"));
+
+      $sourceWrap
+        .on("dragover", dragFileStart)
+        .on("dragleave", dragFileReset)
+        .on("drop", dragFileLoading);
+    }
   };
 
   // source Ele (textarea)
   var sourceEle = document.getElementById("sourceData");
   MapGeneratorTableize(sourceEle);
+  initSourceDragDrop();
 })(jQuery)
